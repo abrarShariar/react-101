d16f067aa8a1d7840adc33351018fc3848feebc8
abrar <shariarabrar@gmail.com>
2017-07-31 17:15:53 +0600

    need to work on data flow from child to parent and sibling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 //my components
@@ -9,33 +8,39 @@ import LanguageAdder from './components/LangugeAdder';
 
 
 class App extends Component {
-  render() {
-    return (
-    <div className="App">
-        <div className="col-md-12">
-            <div className="col-md-6">
-                <h1>Add Programming Languages</h1>
-                <hr/>
-                <LanguageAdder/>
-            </div>
-            <div className="col-md-6">
 
+    onNewLangAdd = (newLang) => {
+        console.log("From App Component");
+        console.log("Hello World");
+    }
+
+    render() {
+        return (
+        <div className="App">
+            <div className="col-md-12">
+                <div className="col-md-6">
+                    <h1>Add Programming Languages</h1>
+                    <hr/>
+                    <LanguageAdder onNewLangAdd={this.onNewLangAdd}/>
+                </div>
+                <div className="col-md-6">
+
+                </div>
+            </div>
+
+            <div className="col-md-12">
+                <div className="col-md-6">
+                    <h1>Filter By Language</h1>
+                    <hr/>
+                    <Filter/>
+                </div>
+                <div className="col-md-6">
+                    <h1>Search Github</h1>
+                    <hr/>
+                    <SearchBar/>
+                </div>
             </div>
         </div>
-
-        <div className="col-md-12">
-            <div className="col-md-6">
-                <h1>Filter By Language</h1>
-                <hr/>
-                <Filter/>
-            </div>
-            <div className="col-md-6">
-                <h1>Search Github</h1>
-                <hr/>
-                <SearchBar/>
-            </div>
-        </div>
-    </div>
     );
   }
 }
--- src/components/LangugeAdder.js
+++ src/components/LangugeAdder.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
 
-class LangugeAdder extends React.Component{
+class LanguageAdder extends React.Component{
+
+    languageList = [];
+
+    constructor(props){
+        super(props);
+        this.state = {
+            langList: this.languageList
+        }
+        this.addLang = this.addLang.bind(this);
+    }
+
+    onNewLangAdded(){
+        console.log("on new lang add from onNewLangAdded");
+    }
+
+
+    myFoo = () => {
+        this.props.onNewLangAdd(listInfo);
+
+    }
+
+    addLang(event) {
+       let input = document.getElementById('lang-input').value;
+       this.languageList.push(input);
+
+       this.setState({
+           langList: this.languageList
+       })
+    }
+
     render(){
         return(
             <div className="LanguageAdder">
                 <div className="form-group">
-                    <input className="form-control" type="text" />
-                    <button className="btn btn-success" id="add-btn">ADD</button>
+                    <input className="form-control" type="text" id="lang-input" />
+                    <button className="btn btn-success" id="add-btn" onClick={this.addLang}>ADD</button>
                 </div>
             </div>
         )
     }
 }
 
-export default LangugeAdder;
+export default LanguageAdder;
